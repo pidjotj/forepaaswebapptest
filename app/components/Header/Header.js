e4bd34f53e8e8be03c271cbda52d95baaa8b0012
Jerem <jeremy1.pidjot@epitech.eu>
2019-07-30 15:22:56 +0200

    init: Downgrade material.ui (hooks bug) + header struct ok

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import Image from './images/ticket.svg';
 import './style.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Header extends React.Component {
   render() {
+    // TODO Creer un ficher constante
     const title = 'MovieFinder';
+    const home = 'Home';
+    const search = 'Search';
     return (
-      <div className="flex items-center mt-8">
-        <img src={Image} alt="Logo" className="w-16" />
-        <span className="text-black text-xl ml-8">{title}</span>
+      <div className="flex items-center mt-8 relative">
+        <div className="flex items-center">
+          <img src={Image} alt="Logo" className="w-16" />
+          <span className="text-black text-xl ml-8">{title}</span>
+        </div>
+        <div className="flex items-center absolute right-0">
+          <Button color="primary">
+            {home}
+          </Button>
+          <Button color="primary">
+            {search}
+          </Button>
+        </div>
       </div>
     );
   }
