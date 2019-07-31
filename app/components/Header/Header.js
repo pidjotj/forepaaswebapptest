9cea9ce9efbbd76659e0ec321c83d4b22e54e282
Jerem <jeremy1.pidjot@epitech.eu>
2019-07-31 17:36:56 +0200

    style: Add logo in the header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import Image from './images/ticket.svg';
+import Image from '../../images/logo.svg';
 import './style.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Header extends React.Component {
   render() {
     // TODO Creer un ficher constante
-    const title = 'MovieFinder';
     const home = 'Home';
     const search = 'Search';
     return (
       <div className="flex items-center mt-8 relative">
         <div className="flex items-center">
-          <img src={Image} alt="Logo" className="w-16" />
-          <span className="text-black text-xl ml-8">{title}</span>
+          <img src={Image} alt="Logo" className="w-auto" />
         </div>
         <div className="flex items-center absolute right-0 mr-8">
           <Link to="/">
